67db4f84e176795eed49ad53e29e009ebbbb1706
Anurag769 <97097499+Anurag769@users.noreply.github.com>
2022-01-14 13:25:40 +0530

    Add files via upload

---
+++ assignment computing OUTPUT.py
@@ -0,0 +1,35 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:\Users\HP\OneDrive\Desktop\assignment computing INPUT.py =====
+ANSWER 1.
+Enter 1st No = 6
+Enter 2nd No = 7
+Enter 3rd No = 17
+10.0
+ANSWER 2.
+Enter Your Gross Income (in nearest integer) = 1000000
+Enter No. Of Dependent = 5
+Your Total Income Tax is :  195000.0
+ANSWER 3.
+Your SID : 21104106
+Your Name : ANURAG GARG
+For Male (M),For Female(F),For Unknown(U)
+Your Gender : M
+Your Course Name : ELECTRICAL
+Your CGPA : 10
+[21104106, 'ANURAG GARG', 'M', 'ELECTRICAL', 10.0]
+ANSWER 4.
+Enter marks of 5 students
+98
+77
+89
+67
+97
+[67.0, 77.0, 89.0, 97.0, 98.0]
+ANSWER 5.A
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+ANSWER 5.B
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
