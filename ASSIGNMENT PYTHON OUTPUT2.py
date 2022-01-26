d5db6c33575580c0bd8a7748bc99ff9c32a1e4d5
Anurag769 <97097499+Anurag769@users.noreply.github.com>
2022-01-26 17:32:31 +0530

    Add files via upload

---
+++ ASSIGNMENT PYTHON OUTPUT2.py
@@ -0,0 +1,61 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/HP/OneDrive/Desktop/ASSIGNMENT PYTHON INPUT 2 =======
+
+Question-1
+
+String Given : Python is a case sensitive language
+Length of String : 35
+String in Reversed Order : egaugnal evitisnes esac a si nohtyP
+New String : a case sensitive
+Updated String : Python is object oriented language
+Index of Substring "a" : 10
+String without White Spaces : Pythonisacasesensitivelanguage
+
+
+Question-2
+
+Type your Name: ANURAG GARG
+Type your SID: 21104106
+Type your Department's Name: ELECTRICAL 
+Type your CGPA: 10
+
+Hey ANURAG GARG Here!
+My SID is 21104106
+I am from ELECTRICAL  department and my CGPA is 10.0.
+
+
+Question-3
+
+Value of a = 56
+Value of b = 10
+a & b = 8
+a | b = 58
+a ^ b = 50
+Shifting both a and b with 2 bits to the Left : a-224,b-40
+Shifting a with 2 and b with 4 bits to the Right : a-14,b-0
+
+
+Question-4
+
+Type First Number: 56
+Type Second Number: 44
+Type Third Number: 69
+Greatest of the Three Numbers entered is : 69.0
+
+
+Question-5
+
+Type the String : 'OUR MID SEMS ARE GOING TO START'
+No
+
+
+Question-6
+
+Enter the Length of first side: 40
+Enter the Length of second side: 25
+Enter the Length of third side: 33
+Yes
+
+
