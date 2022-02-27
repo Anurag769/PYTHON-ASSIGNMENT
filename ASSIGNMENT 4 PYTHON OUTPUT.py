7f644225cbf41738e243597644ef83f009649ec0
Anurag769 <97097499+Anurag769@users.noreply.github.com>
2022-02-27 15:08:26 +0530

    Add files via upload

---
+++ ASSIGNMENT 4 PYTHON OUTPUT.py
@@ -0,0 +1,57 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/ASSIGNMENT 4 PYTHON INPUT.py ======
+Q-1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+Q-2
+Enter the number of rows: 4
+The Pascal's Triangle using recursion is:
+   1 
+  1 1 
+ 1 2 1 
+1 3 3 1 
+Q-3
+Enter first number: 34
+Enter second number: 78
+(A) Checking if the quotient and remainder is a callable value or not.
+False
+False
+(B)
+The quotient is zero
+(C): Numbers that are greater than 4 are : [38, 39, 5, 39, 6, 40]
+(D): Set: {5, 6, 38, 40, 39}
+(E): Immutable set: frozenset({5, 6, 38, 40, 39})
+(F): Hash value of the max value from the above set: 40
+
+
+Q-4
+Enter name: ANURAG GARG
+Enter roll no. of ANURAG GARG: 21104106
+Student details are here..
+The name of the student is ANURAG GARG and roll number is 21104106
+The object is destroyed
+
+
+Q-5
+ Name of the Employee: Mehak
+        Salary: 40000
+ Name of the Employee: Ashok
+        Salary: 50000
+ Name of the Employee: Viren
+        Salary: 60000
+(a)
+****Updated salary of Mehak****
+ Name of the Employee: Mehak
+        Salary: 70000
+(b)
+The record of viren is deleted
+
+
+Enter the word: 
